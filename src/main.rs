b9673af27d28f2368237cc2890844a6830c779a2
Marcelo <mrcelo014@gmail.com>
2024-05-04 14:34:09 -0300

    sla

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::io::{self, Write};
 
 use rand::seq::SliceRandom;
 
@@ -8,17 +8,78 @@ fn selected_rand_keyword() -> &'static str {
     keyword
 }
 
+fn check_attempt(attempt: &mut String, keyword: &str, hint: &mut Vec<String>) -> bool {
+    let replace_char = attempt.trim().chars().next().expect("Error: Replace char");
+
+    if keyword.contains(attempt.trim()) {
+        for (i, word) in keyword.chars().enumerate() {
+            if word == replace_char {
+                hint[i] = attempt.trim().to_string()
+            }
+        }
+        return true;
+    }
+    false
+}
+
+fn clear_scream() {
+    print!("{esc}c", esc = 27 as char);
+}
+
+fn test() {
+    println!("Slaaaa");
+}
+
 fn main() {
     let mut input_user = String::new();
+    let mut hits: Vec<String> = Vec::new();
+    let mut attempts = 0;
+    let keyword = selected_rand_keyword();
+    let mut message = String::from("Digite uma letra.");
 
-    println!("Jogo da forca, descubra a palavra pela sua vida!!!");
+
+    for _ in 0..keyword.chars().count() {
+        hits.push(" _".to_string());
+    }
 
     loop {
-        let keyword = selected_rand_keyword();
+        clear_scream();
+        println!("Jogo da forca, descubra a palavra pela sua vida!!!");
+        println!("{}", hits.join(""));
+
+        println!("{}", message);
+        input_user.clear();
+        io::stdout().flush().expect("Error clear buffer");
+
         io::stdin().read_line(&mut input_user).expect("Invalid input");
-        println!("{}", keyword);
-        if input_user.contains("sair") {
+
+        if attempts == 5 {
+            println!("Você perdeu!!!");
             break;
         }
+
+        if input_user.trim().len() > 1 {
+            message.clear();
+            message.push_str("Digite apenas uma letra!!!");
+            attempts -= 1;
+            continue;
+        }
+
+        println!("{}", keyword);
+        
+        let checked_attempt = check_attempt(&mut input_user, &keyword, &mut hits);
+
+        if checked_attempt {
+            let mut guess = String::new();
+            io::stdin().read_line(&mut guess).expect("Invalid input");
+            
+            if keyword.eq(guess.trim()) {
+                test();
+                println!("Você acertou!!!");
+                break;
+            }
+            message.push_str("Você errou, tente novamente.");
+        }
+        
     }
 }
